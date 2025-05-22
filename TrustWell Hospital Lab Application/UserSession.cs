63b67f7c6efa5260c25c4dd658886720de5e78e5
kpabasara12003 <kakpjayawickrama@students.nsbm.ac.lk>
2025-05-22 06:43:48 +0530

    update
    check

---
+++ TrustWell Hospital Lab Application/UserSession.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    public static class UserSession
+    {
+        public static int UserId;
+        public static string Username;
+        public static string Role;
+        public static string StaffId;
+    }
+}

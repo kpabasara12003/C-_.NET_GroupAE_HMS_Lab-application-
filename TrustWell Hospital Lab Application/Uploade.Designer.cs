1e2202e54dfd50ed81626bef11e72ffa216a464c
Nisith Nimtharu Welivitiya <155635480+NisithNim2007@users.noreply.github.com>
2025-06-03 15:24:58 +0530

    read only update
    1111

--- TrustWell Hospital Lab Application/Uploade.Designer.cs
+++ TrustWell Hospital Lab Application/Uploade.Designer.cs
@@ -53,9 +53,10 @@
             this.label1.BackColor = System.Drawing.Color.White;
             this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.Red;
-            this.label1.Location = new System.Drawing.Point(59, 466);
+            this.label1.Location = new System.Drawing.Point(44, 379);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(289, 28);
+            this.label1.Size = new System.Drawing.Size(230, 21);
             this.label1.TabIndex = 5;
             this.label1.Text = "Select the Report and submit";
             // 
@@ -66,10 +67,10 @@
             this.panelone.Controls.Add(this.doctxt);
             this.panelone.Controls.Add(this.gunaButton1);
             this.panelone.Controls.Add(this.subbut);
-            this.panelone.Location = new System.Drawing.Point(80, 92);
+            this.panelone.Location = new System.Drawing.Point(60, 75);
             this.panelone.Margin = new System.Windows.Forms.Padding(2);
             this.panelone.Name = "panelone";
-            this.panelone.Size = new System.Drawing.Size(890, 193);
+            this.panelone.Size = new System.Drawing.Size(668, 157);
             this.panelone.TabIndex = 4;
             // 
             // doctxt
@@ -82,13 +83,14 @@
             this.doctxt.FocusedBorderColor = System.Drawing.Color.Black;
             this.doctxt.FocusedForeColor = System.Drawing.SystemColors.ControlText;
             this.doctxt.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
-            this.doctxt.Location = new System.Drawing.Point(22, 19);
+            this.doctxt.Location = new System.Drawing.Point(16, 15);
             this.doctxt.Margin = new System.Windows.Forms.Padding(2);
             this.doctxt.Name = "doctxt";
             this.doctxt.PasswordChar = '\0';
             this.doctxt.Radius = 7;
+            this.doctxt.ReadOnly = true;
             this.doctxt.SelectedText = "";
-            this.doctxt.Size = new System.Drawing.Size(601, 46);
+            this.doctxt.Size = new System.Drawing.Size(451, 37);
             this.doctxt.TabIndex = 0;
             this.doctxt.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             this.doctxt.TextChanged += new System.EventHandler(this.doctxt_TextChanged);
@@ -106,7 +108,7 @@
             this.gunaButton1.ForeColor = System.Drawing.Color.White;
             this.gunaButton1.Image = null;
             this.gunaButton1.ImageSize = new System.Drawing.Size(20, 20);
-            this.gunaButton1.Location = new System.Drawing.Point(391, 101);
+            this.gunaButton1.Location = new System.Drawing.Point(293, 82);
             this.gunaButton1.Margin = new System.Windows.Forms.Padding(2);
             this.gunaButton1.Name = "gunaButton1";
             this.gunaButton1.OnHoverBaseColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(117)))), ((int)(((byte)(172)))));
@@ -115,7 +117,7 @@
             this.gunaButton1.OnHoverImage = null;
             this.gunaButton1.OnPressedColor = System.Drawing.Color.Black;
             this.gunaButton1.Radius = 15;
-            this.gunaButton1.Size = new System.Drawing.Size(217, 57);
+            this.gunaButton1.Size = new System.Drawing.Size(163, 46);
             this.gunaButton1.TabIndex = 1;
             this.gunaButton1.Text = "Select";
             this.gunaButton1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
@@ -134,7 +136,7 @@
             this.subbut.ForeColor = System.Drawing.Color.White;
             this.subbut.Image = null;
             this.subbut.ImageSize = new System.Drawing.Size(20, 20);
-            this.subbut.Location = new System.Drawing.Point(651, 101);
+            this.subbut.Location = new System.Drawing.Point(488, 82);
             this.subbut.Margin = new System.Windows.Forms.Padding(2);
             this.subbut.Name = "subbut";
             this.subbut.OnHoverBaseColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(117)))), ((int)(((byte)(172)))));
@@ -143,7 +145,7 @@
             this.subbut.OnHoverImage = null;
             this.subbut.OnPressedColor = System.Drawing.Color.Black;
             this.subbut.Radius = 15;
-            this.subbut.Size = new System.Drawing.Size(217, 57);
+            this.subbut.Size = new System.Drawing.Size(163, 46);
             this.subbut.TabIndex = 3;
             this.subbut.Text = "Submit";
             this.subbut.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
@@ -154,10 +156,10 @@
             this.paneltwo.AutoSize = true;
             this.paneltwo.BackColor = System.Drawing.Color.White;
             this.paneltwo.Controls.Add(this.valbut);
-            this.paneltwo.Location = new System.Drawing.Point(80, 326);
+            this.paneltwo.Location = new System.Drawing.Point(60, 265);
             this.paneltwo.Margin = new System.Windows.Forms.Padding(2);
             this.paneltwo.Name = "paneltwo";
-            this.paneltwo.Size = new System.Drawing.Size(657, 103);
+            this.paneltwo.Size = new System.Drawing.Size(493, 84);
             this.paneltwo.TabIndex = 2;
             this.paneltwo.Visible = false;
             // 
@@ -171,13 +173,13 @@
             this.valbut.FocusedBorderColor = System.Drawing.Color.Black;
             this.valbut.FocusedForeColor = System.Drawing.SystemColors.ControlText;
             this.valbut.Font = new System.Drawing.Font("Segoe UI", 9F);
-            this.valbut.Location = new System.Drawing.Point(22, 26);
+            this.valbut.Location = new System.Drawing.Point(16, 21);
             this.valbut.Margin = new System.Windows.Forms.Padding(2);
             this.valbut.Name = "valbut";
             this.valbut.PasswordChar = '\0';
             this.valbut.Radius = 7;
             this.valbut.SelectedText = "";
-            this.valbut.Size = new System.Drawing.Size(575, 43);
+            this.valbut.Size = new System.Drawing.Size(431, 35);
             this.valbut.TabIndex = 0;
             this.valbut.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
@@ -188,8 +190,9 @@
             this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel1.ForeColor = System.Drawing.Color.White;
             this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1920, 100);
+            this.panel1.Size = new System.Drawing.Size(1440, 81);
             this.panel1.TabIndex = 6;
             // 
             // label2
@@ -198,9 +201,10 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(781, 22);
+            this.label2.Location = new System.Drawing.Point(586, 18);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(262, 38);
+            this.label2.Size = new System.Drawing.Size(208, 30);
             this.label2.TabIndex = 0;
             this.label2.Text = "Lab Report Upload";
             this.label2.Click += new System.EventHandler(this.label2_Click);
@@ -213,9 +217,10 @@
             this.panel2.Controls.Add(this.button1);
             this.panel2.Controls.Add(this.cuiPanel1);
             this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel2.Location = new System.Drawing.Point(0, 100);
+            this.panel2.Location = new System.Drawing.Point(0, 81);
+            this.panel2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(1920, 666);
+            this.panel2.Size = new System.Drawing.Size(1440, 541);
             this.panel2.TabIndex = 7;
             // 
             // button1
@@ -231,7 +236,8 @@
             this.button1.ForeColor = System.Drawing.Color.White;
             this.button1.Image = null;
             this.button1.ImageSize = new System.Drawing.Size(20, 20);
-            this.button1.Location = new System.Drawing.Point(47, 35);
+            this.button1.Location = new System.Drawing.Point(35, 28);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
             this.button1.OnHoverBaseColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
             this.button1.OnHoverBorderColor = System.Drawing.Color.Black;
@@ -239,7 +245,7 @@
             this.button1.OnHoverImage = null;
             this.button1.OnPressedColor = System.Drawing.Color.Black;
             this.button1.Radius = 8;
-            this.button1.Size = new System.Drawing.Size(166, 53);
+            this.button1.Size = new System.Drawing.Size(124, 43);
             this.button1.TabIndex = 1;
             this.button1.Text = "Go Back";
             this.button1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
@@ -255,24 +261,26 @@
             this.cuiPanel1.Controls.Add(this.label1);
             this.cuiPanel1.Controls.Add(this.panelone);
             this.cuiPanel1.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.cuiPanel1.Location = new System.Drawing.Point(437, 35);
+            this.cuiPanel1.Location = new System.Drawing.Point(328, 28);
+            this.cuiPanel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.cuiPanel1.Name = "cuiPanel1";
             this.cuiPanel1.OutlineThickness = 1F;
-            this.cuiPanel1.Padding = new System.Windows.Forms.Padding(10);
+            this.cuiPanel1.Padding = new System.Windows.Forms.Padding(8, 8, 8, 8);
             this.cuiPanel1.PanelColor = System.Drawing.Color.White;
             this.cuiPanel1.PanelOutlineColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(117)))), ((int)(((byte)(172)))));
             this.cuiPanel1.Rounding = new System.Windows.Forms.Padding(8);
-            this.cuiPanel1.Size = new System.Drawing.Size(1037, 525);
+            this.cuiPanel1.Size = new System.Drawing.Size(777, 427);
             this.cuiPanel1.TabIndex = 0;
             // 
             // Uploade
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Uploade";
-            this.Size = new System.Drawing.Size(1920, 766);
+            this.Size = new System.Drawing.Size(1440, 622);
             this.Load += new System.EventHandler(this.Uploade_Load);
             this.panelone.ResumeLayout(false);
             this.paneltwo.ResumeLayout(false);
